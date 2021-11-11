bdf6e7cdcb3aea18ebac7919c0e48a34ef81185e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2021-11-11 11:30:51 +0500

    Update nav-menu.js

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -15,25 +15,3 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-// slider - section
-
-const slider = document.querySelector('.slider');
-const left = document.querySelector('.left');
-const slide = document.querySelector('.slide')
-const right = document.querySelector('.right');
-var sectionIndex = 0;
-right.addEventListener('click', function () {
-    // sectionIndex = (sectionIndex < 2) ?  sectionIndex + 1 : 2;
-
-    if (sectionIndex = sectionIndex < 2) {
-        sectionIndex + 1;
-    }
-    else {
-        sectionIndex + 2;
-    }
-    slider.style.transform = "translate(" + (sectionIndex = sectionIndex + 1) * -33 + "%)";
-});
-left.addEventListener('click', function () {
-    slider.style.transform = "translate(0%)";
-
-});
